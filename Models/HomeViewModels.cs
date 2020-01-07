0a4b1f71b4b72a493504d6472e12a17eff5f47c5
Froggyfrogs <42100071+Froggyfrogs@users.noreply.github.com>
2020-01-07 19:12:43 +0100

    indexList

---
+++ Models/HomeViewModels.cs
@@ -0,0 +1,13 @@
+﻿using LuvDating.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuvDating.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<ApplicationUser> UserList { get; set; }
+    }
+}
