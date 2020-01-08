bbd9f4c41eec8a47ba4305ad0eceaef6897be00e
Ludwig <unconfigured@null.spigotmc.org>
2020-01-08 16:08:38 +0100

    La in webapi

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,3 +1,4 @@
+using LuDating.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace LuvDating
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
