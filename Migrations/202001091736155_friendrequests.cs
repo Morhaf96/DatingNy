2926ab68c01c415f82153f2c81875f2c414fcecf
Froggyfrogs <42100071+Froggyfrogs@users.noreply.github.com>
2020-01-09 18:54:28 +0100

    Första steget för vänner
    Kan skicka friendrequests, inte acceptera eller neka än

---
+++ Migrations/202001091736155_friendrequests.cs
@@ -0,0 +1,16 @@
+namespace LuvDating.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class friendrequests : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
