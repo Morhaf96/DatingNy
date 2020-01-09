ebf27284636771ed11d0d306961fda427abb9084
Ludwig <unconfigured@null.spigotmc.org>
2020-01-09 13:57:38 +0100

    Is me ye boi here with another commit for the children

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -17,22 +17,30 @@ namespace LuvDating.Controllers
             var userId = User.Identity.GetUserId();
             var userInfo = db.Users.FirstOrDefault(a => a.Id == userId);
 
-            return View(new ProfileIndexViewModel
+            try
             {
-                AccountId = userId,
-                Name = userInfo.Name,
-                Birth = userInfo.BirthDate,
-                Gender = userInfo.Gender,
-                Bio = userInfo.Bio,
-                Image = userInfo.ImageName,
-            });
+                return View(new ProfileIndexViewModel
+                {
+                    AccountId = userId,
+                    Name = userInfo.Name,
+                    Birth = userInfo.BirthDate,
+                    Gender = userInfo.Gender,
+                    Bio = userInfo.Bio,
+                    Image = userInfo.ImageName,
+                });
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
         }
-        public ActionResult UserProfile(string profileId)
+        public ActionResult UserProfile(string id)
         {
             var db = new ApplicationDbContext();
-            var chosenProfile = db.Users.FirstOrDefault(p => p.Id == profileId);
+            var chosenProfile = db.Users.FirstOrDefault(p => p.Id == id);
             var currentUser = User.Identity.GetUserId();
-            if(profileId == currentUser)
+            if(id == currentUser)
             {
                 return RedirectToAction("Index");
             }
