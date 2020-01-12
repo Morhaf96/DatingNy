d1adfda162df0ccff49cd068991a5497603ea2b1
Ludwig <unconfigured@null.spigotmc.org>
2020-01-12 20:20:55 +0100

    Hyfsad

--- Global.asax.cs
+++ Global.asax.cs
@@ -12,13 +12,14 @@ namespace LuvDating
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
