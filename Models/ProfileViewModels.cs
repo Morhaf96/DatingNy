5f5b5558cf74212273ed155b60024494f09ad900
Ludwig <unconfigured@null.spigotmc.org>
2020-01-12 20:28:53 +0100

    fsdfsdf

--- Models/ProfileViewModels.cs
+++ Models/ProfileViewModels.cs
@@ -25,5 +25,95 @@ namespace LuvDating.Models
         public List<ApplicationUser> FriendList { get; set; }
 
     }
-    
+
+    public class ProfileEditViewModel
+    {
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        [RegularExpression("^([a-zA-Zåäö]{2,}\\s[a-zA-zåäö]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Zåäö]{1,})?)", ErrorMessage = "Write a correct first and last name!")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please enter your gender")]
+        public string Gender { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [Required(ErrorMessage = "Please enter your birthdate")]
+        [DateMinimumAge(18)]
+        [DateMaximumAge(100)]
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birth { get; set; }
+
+        [Display(Name = "Bio (Optional)")]
+        public string Bio { get; set; }
+    }
+
+    public class SenderListModel
+    {
+        public List<ApplicationUser> RequestsFrom { get; set; }
+    }
+
+
+    //Validerings klasser för ålder.
+    public class DateMinimumAgeAttribute : ValidationAttribute
+    {
+        public DateMinimumAgeAttribute(int minimumAge)
+        {
+            MinimumAge = minimumAge;
+            ErrorMessage = "{0} must be someone at least {1} years of age";
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if ((value != null && DateTime.TryParse(value.ToString(), out date)))
+            {
+                return date.AddYears(MinimumAge) < DateTime.Now;
+            }
+
+            return false;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(ErrorMessageString, name, MinimumAge);
+        }
+
+        public int MinimumAge { get; }
+    }
+
+    public class DateMaximumAgeAttribute : ValidationAttribute
+    {
+        public DateMaximumAgeAttribute(int maximumAge)
+        {
+            MaximumAge = maximumAge;
+            ErrorMessage = "{0} can't be over {1} years of age";
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if ((value != null && DateTime.TryParse(value.ToString(), out date)))
+            {
+                return date.AddYears(MaximumAge) > DateTime.Now;
+            }
+
+            return false;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(ErrorMessageString, name, MaximumAge);
+        }
+
+        public int MaximumAge { get; }
+    }
+
 }
