1309c58c8cebe513ab25e2f16f27143810cb1c8f
Froggyfrogs <42100071+Froggyfrogs@users.noreply.github.com>
2020-01-13 20:46:30 +0100

    friendmodel moved

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -49,23 +49,5 @@ namespace LuvDating.Models
         }
     }
 
-    public class FriendModel
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Key]
-        public int FriendId { get; set; }
-        public string FriendRequestReciever { get; set; }
-
-        public int pendingRequest { get; set; }
-
-        public string Name { get; set; }
-
-        [ForeignKey("Users")]
-        public virtual ICollection<ApplicationUser> Sender { get; set; }
-
-        public FriendModel()
-        {
-            this.Sender = new HashSet<ApplicationUser>();
-        }
-    }
+   
 }
---
+++ Models/FriendModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LuvDating.Models
+{
+    public class FriendModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int FriendId { get; set; }
+        public string FriendRequestReciever { get; set; }
+
+        public int pendingRequest { get; set; }
+
+        public string Name { get; set; }
+
+        [ForeignKey("Users")]
+        public virtual ICollection<ApplicationUser> Sender { get; set; }
+
+        public FriendModel()
+        {
+            this.Sender = new HashSet<ApplicationUser>();
+        }
+    }
+}
